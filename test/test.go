a500fb482dbb89732995ea4ab7cc85aad176a3fd
Bjørn Fjukstad <bfj001@post.uit.no>
2014-05-01 14:23:34 +0200

    new tests for new implementation

--- test/test.go
+++ test/test.go
@@ -1,42 +1,43 @@
 package main
 
-import ( 
-    "fmt"
-    "math/rand"
-    "time" 
-    "github.com/fjukstad/rpcman"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fjukstad/rpcman"
 )
 
+func genrands(num int) (ret []float64) {
 
-func genrands(num int) (ret [] float64) { 
-
-    ret = make([] float64, num)
+	ret = make([]float64, num)
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-    for i := 0; i < num; i++ {
-        ret[i] = r.Float64() * 100
-    }
-    return ret
-    
-} 
+	for i := 0; i < num; i++ {
+		ret[i] = r.Float64() * 100
+	}
+	return ret
 
-
+}
 
 func main() {
-    rpc := rpcman.Init("tcp://localhost:5555") 
+	rpc, err := rpcman.Init("tcp://localhost:5555")
+	if err != nil {
+		return
+	}
 
-    for i := 0; i < 10; i++ {
-        floats := genrands(100)
-        sum,_ := rpc.Call("sum", floats)
-        std,_ := rpc.Call("std", floats)
-        variance,_ := rpc.Call("var", floats) 
-        mean,_ := rpc.Call("mean", floats) 
-        
-        add, _ := rpc.Call("add", 2,3)
+	for i := 0; i < 10; i++ {
+		floats := genrands(100)
+		sum, _ := rpc.Call("sum", floats)
+		std, _ := rpc.Call("std", floats)
+		variance, _ := rpc.Call("var", floats)
+		mean, _ := rpc.Call("mean", floats)
 
-        fmt.Println(sum,std,variance,mean,add) 
-    }
+		add, _ := rpc.Call("add", 2, 3)
 
-    rpc.Close()
+		fmt.Println(sum, std, variance, mean, add)
+	}
 
-} 
+	// rpc.Close()
+
+}
