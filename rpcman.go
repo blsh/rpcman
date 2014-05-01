601ceb2fdebc32a8b67b922827f583f0f8026e53
Bjørn Fjukstad <bfj001@post.uit.no>
2014-05-01 14:18:43 +0200

    Now using a single socket.
    Obviously zeromq does not like when mulitple goroutines use the same
    socket. Old implementation opened a socket on every Call() but (and this is only
    speculation) if go opens and closes sockets too frequently things will go
    haywire.
    
    New implementation uses one socket per rpcman, so you'll have to make sure that
    rpc.Call()'s are behind some kind of mutex or lock.

--- rpcman.go
+++ rpcman.go
@@ -1,95 +1,107 @@
 package rpcman
 
 import (
-    "log"
-    "errors"
-    "encoding/json"    
-    "time"
-    zmq "github.com/alecthomas/gozmq" 
+	"encoding/json"
+	"errors"
+	"log"
+	"sync"
+	"time"
 
+	zmq "github.com/alecthomas/gozmq"
 )
-type RPCMan struct { 
-    Context *zmq.Context
-    ServAddr string 
-} 
-    
+
+type RPCMan struct {
+	Context  *zmq.Context
+	ServAddr string
+	Socket   *zmq.Socket
+	mutex    *sync.Mutex
+}
 
 type Request struct {
-    Method string
-    Args interface{}
-} 
+	Method string
+	Args   interface{}
+}
 
 type Response struct {
-    Response interface{}
-    Status int
-} 
+	Response interface{}
+	Status   int
+}
 
-func Init(addr string) RPCMan { 
-    context, _ := zmq.NewContext()
-    return RPCMan{context,addr} 
-} 
+func Init(addr string) (*RPCMan, error) {
+	context, _ := zmq.NewContext()
+	major, minor, patch := zmq.Version()
 
+	socket, err := context.NewSocket(zmq.REQ)
+	if err != nil {
+		log.Println("could not set up socket to rpcman", err)
+		return nil, err
+	}
+	err = socket.Connect(addr)
+	if err != nil {
+		log.Println("Could not connect to socket", err)
+		return nil, err
+	}
 
-func (rpc RPCMan) Close() {
-    defer rpc.Context.Close()
-}  
+	// set timeout to 1s
+	duration, _ := time.ParseDuration("1s")
+	socket.SetRcvTimeout(duration)
 
-func (rpc RPCMan) Call(method string, args ...interface{}) (interface{}, error){ 
-    
+	mutex := &sync.Mutex{}
+	//cond := sync.NewCond(sync.Mutex{})
 
-    msg := Request{method, args} 
+	rpc := RPCMan{
+		Context:  context,
+		ServAddr: addr,
+		Socket:   socket,
+		mutex:    mutex}
 
-    enc, err := json.Marshal(msg)
-    if err != nil {
-        log.Println("json error", err) 
-        return -1, err        
-    } 
-    
-    socket, err := rpc.Context.NewSocket(zmq.REQ)
-    if err != nil {
-        log.Println("could not set up socket to rpcman",err)
-        return -1, err
-    }
-
-    // set timeout to 1s
-    duration, _ := time.ParseDuration("1s")
-    socket.SetRcvTimeout(duration)
-    
-    err = socket.Connect(rpc.ServAddr)
-    if err != nil {
-        log.Println("Could not connect to socket",err)
-        return -1, err
-    }
-    
-
-    
-    socket.Send(enc,0)
-    //rpc.Socket.Send(enc, 0)
-    resp := new(Response) 
-    
-    //reply, err := rpc.Socket.Recv(0)
-    reply, err := socket.Recv(0)
-    if err != nil {
-        log.Println("RPC recv failed:", err)
-        return -1, err
-    }
-        
-    err = json.Unmarshal(reply,resp) 
-    
-    if err != nil {
-        log.Println("json error", err)
-        return -1, err
-    }
-
-    if resp.Status != 0 {
-        return -1, errors.New("RPC Method not found:"+method) 
-    } 
-
-    socket.Close()
-    
-
-    
-    return resp.Response, nil
+	return &rpc, nil
 
 }
 
+func (rpc RPCMan) Close() {
+	rpc.Context.Close()
+}
+
+func (rpc RPCMan) Call(method string, args ...interface{}) (interface{}, error) {
+
+	// exclusive access to socket
+	//rpc.mutex.Lock()
+	//defer rpc.mutex.Unlock()
+
+	msg := Request{method, args}
+
+	enc, err := json.Marshal(msg)
+	if err != nil {
+		log.Println("json error", err)
+		return -1, err
+	}
+
+	rpc.Socket.Send(enc, 0)
+
+	//rpc.Socket.Send(enc, 0)
+	resp := new(Response)
+
+	//reply, err := rpc.Socket.Recv(0)
+	reply, err := rpc.Socket.Recv(0)
+	if err != nil {
+		log.Println("RPC recv failed:", err, method)
+		return -1, err
+	}
+
+	err = json.Unmarshal(reply, resp)
+
+	if err != nil {
+		log.Println("json error", err)
+		return -1, err
+	}
+
+	if resp.Status != 0 {
+		return -1, errors.New("RPC Method not found:" + method)
+	}
+
+	// socket.Close()
+
+	return resp.Response, nil
+
+}
